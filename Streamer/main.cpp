bff9d3b4ce27c8ef794471c99eea897a60d9d498
LeEmperor <bohdanpurtell@gmail.com>
2025-07-30 02:32:55 +0000

    polygon API phase 1

---
+++ Streamer/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <utility>
+
+#define nL "\n" 
+using namespace std;
+
+
+int main() {
+  cout << "started" << nL;
+
+  cout << "version:" << __cplusplus << nL;
+
+
+  using namespace web;
+  using namespace web::http;
+  using namespace web::http::client;
+
+  // std::string api_key = std::getenv("polygon_api_key");
+  // cout << "api_key: " << api_key << nL;
+  // if (!api_key) {
+  //   perror("api key not here lmao eat ballers");
+  //   exit(EXIT_FAILURE);
+  // }
+
+  std::string api_key = "";
+  std::string symbol = "AAPL";
+
+  // web::uri_builder builder(U("/v2/last/nbbo/" + symbol));
+  web::uri_builder builder(U("/v3/reference/dividends"));
+  builder.append_query(U("apiKey"), api_key);
+
+  web::http::client::http_client client(U("https://api.polygon.io"));
+
+  client.request(
+    web::http::methods::GET,
+    builder.to_string())
+      .then([](web::http::http_response response) {
+          if (response.status_code() == web::http::status_codes::OK) {
+            return response.extract_json();
+          } else {
+            std::cerr << "req failed lmao, status code: " << response.status_code() << nL;
+            return pplx::task_from_result(web::json::value());
+          }
+      })
+
+      .then([](web::json::value json_result) {
+        std::cout << "response: " << json_result.serialize() << nL;
+      })
+    .wait();
+
+  return 0;
+}
+
