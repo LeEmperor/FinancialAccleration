4228d0db3f83b5658e7976a3a7e70faa2bc3ef03
LeEmperor <bohdanpurtell@gmail.com>
2025-09-21 22:14:03 -0400

    nothingburger

---
+++ TestEnvironments/orderbook_test1/controller.sv
@@ -0,0 +1,17 @@
+// Bohdan Purtell
+// University of Florida
+// Controller for Orderbook
+
+module controller # (
+  parameter int width = 32
+) (
+
+  input logic clk, rst, en,
+  output logic dataout,
+
+
+  output logic wren
+);
+
+endmodule
+
---
+++ TestEnvironments/orderbook_test1/toplevel.sv
@@ -0,0 +1,15 @@
+// Bohdan Purtell
+// University of Florida
+// Toplevel for Orderbook
+
+module topleve # (
+  parameter int width = 32
+) (
+  input logic clk, rst, en,
+  output logic tx
+);
+
+
+
+endmodule
+
