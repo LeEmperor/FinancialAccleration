52dce435281c1254688b23e6e6ff978fb695b2c3
LeEmperor <bohdanpurtell@gmail.com>
2025-12-31 03:53:24 -0500

    messed with configs for a while

--- nanoconda/dune_proj/bin/main.ml
+++ nanoconda/dune_proj/bin/main.ml
@@ -3,15 +3,24 @@ msg type: ADD, DELETE, MODIFY
 msg len: 
 sequence context: 
 payload fields:
- *)
+*)
 
 (* module Bruh = struct *)
 type enum_order = ADD | DELETE | MODIFY
 (* end *)
 
-(* type order = { *)
-(*   t: enum_order *)
-(* } *)
+type order = {
+  t: enum_order
+}
+
+type side = Buy | Sell
+
+type add = {
+  order_id : int64;
+  side : side;
+  price : int64;
+  qty : int32;
+}
 
 let _string_of_type_enum = function
   | ADD -> "add";
@@ -19,15 +28,16 @@ let _string_of_type_enum = function
   | MODIFY -> "modify"
 
 let _makeAdd = 
-
   ()
 
 let () = 
-  print_endline "Bonjour, das Welt!"
+  print_endline "Bonjour, das Welt!";
 
-  let type_inst = ADD in
-  let bruh: string = _string_of_type_enum type_inst in
-  Printf.printf "mapped: %s" bruh;
+  let _add_inst : enum_order = ADD in
+  let _del_inst : enum_order = DELETE in
+  let _mod_inst : enum_order = MODIFY in
+  let bruh : string = _string_of_type_enum _del_inst in
+  Printf.printf "mapped: %s\n" bruh;
 
   ()
 
