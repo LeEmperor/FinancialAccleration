7085fe6967094fafbed4d9212c90d40c6f24c4f2
LeEmperor <bohdanpurtell@gmail.com>
2025-12-31 02:16:56 -0500

    CME stuff started

---
+++ nanoconda/dune_proj/_build/default/bin/main.ml
@@ -0,0 +1,37 @@
+(*
+msg type: ADD, DELETE, MODIFY
+msg len: 
+sequence context: 
+payload fields:
+
+
+
+
+ *)
+
+(* module Bruh = struct *)
+type enum_order = ADD | DELETE | MODIFY
+(* end *)
+
+(* type order = { *)
+(*   t: enum_order *)
+(* } *)
+
+let _string_of_type_enum = function
+  | ADD -> "add";
+  | DELETE -> "delete";
+  | MODIFY -> "modify"
+
+let _makeAdd = 
+
+  ()
+
+let () = 
+  print_endline "Bonjour, das Welt!"
+
+  let type_inst = ADD in
+  let bruh: string = _string_of_type_enum type_inst in
+  Printf.printf "mapped: %s" bruh;
+
+  ()
+
